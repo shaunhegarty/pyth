903169201c0f217b53ced4e9c71a8893406623b6
Shaun Hegarty <shaunhegarty@gmail.com>
2022-05-12 02:52:34 +0100

    I think that progress is worth a minor version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 
 setup(name="pyth3",
-      version="0.7",
+      version="0.7.1",
       packages = find_packages(),
       zip_safe = False,
 
